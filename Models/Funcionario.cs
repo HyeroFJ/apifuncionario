0a17f7d716d1101eb95cbd9ec0dad226ac341aa9
Aluno Dev <hyero.jacintho@portalsesisp.org.br>
2024-03-22 10:28:02 -0300

    first

---
+++ Models/Funcionario.cs
@@ -0,0 +1,9 @@
+namespace apifuncionario.Models
+{
+    public class Funcionario
+    {
+        public int FuncionarioID {get;set;}
+        public string? Nome {get;set;}
+        public int SetorID {get;set;}
+    }
+}
